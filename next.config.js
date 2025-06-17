fa883978d3000e95ca9138110f2e9c0532606666
Dheer Tammina <dtammina@gmu.edu>
2025-06-16 21:37:47 -0400

    fixed undici and nextjs issues

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "undici": false,
+      };
+    }
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['undici'],
+  },
 };
 
 module.exports = nextConfig;
